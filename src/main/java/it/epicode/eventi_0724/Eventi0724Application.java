2e7e616d67280cefbbc44743c1b90f7c00b582df
mauro_larese <mauro.larese@omnys.com>
2024-12-16 12:25:05 +0100

    Aggiornati i commenti

--- src/main/java/it/epicode/eventi_0724/Eventi0724Application.java
+++ src/main/java/it/epicode/eventi_0724/Eventi0724Application.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// applicazione Spring Boot generata in automatico
+// contiene il main da lanciare per far partire l'applicazione
+// tutte le classi create devono essere sotto il package principale
+// specificato in questa classe
+// altrimenti non vengono trovate per le dependency injection
 public class Eventi0724Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Eventi0724Application.class, args);
 	}
